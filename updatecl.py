b5ed01247938bbba5ffb5db8ca6a748cb61547de
Anthony Serrato <anthony17@Anthonys-MBP.lan1>
2020-06-23 00:47:38 -0700

    version 1.0

---
+++ updatecl.py
@@ -0,0 +1,40 @@
+from docx import Document
+
+document = Document('cover_letter_template.docx')
+
+company_name =  str(raw_input("Enter the name of the company you are applying for:\n"))
+company_adress = str(raw_input("Enter the adress of the company you are applying for:\n"))
+position_name = str(raw_input("Enter the name of the position you are applying for:\n"))
+position_posting_location = str(raw_input("Where did you find the job posting?:\n"))
+body_number = str(raw_input("Which preset cover letter body would you like to use?:(enter only the number ei. 1,2,3...)\n"))
+delete_mode = False
+keep_mode = False
+
+for paragraph in document.paragraphs:
+    if '[COMPANY NAME]' in paragraph.text:
+        paragraph.text = {company_name}
+    if '[COMPANY ADRESS]' in paragraph.text:
+    	paragraph.text = {company_adress}
+    if '[POSITION NAME]' in paragraph.text:
+    	paragraph.text = paragraph.text.replace("[POSITION NAME]",position_name)
+    if '[POSITION POSTING LOCATION]' in paragraph.text:
+    	paragraph.text = paragraph.text.replace("[POSITION POSTING LOCATION]",position_posting_location)
+    if '<body' in paragraph.text:
+    	if '<body '+body_number in paragraph.text:
+    		keep_mode = True
+    		paragraph._element.getparent().remove(paragraph._element)
+    	else:
+	    	paragraph._element.getparent().remove(paragraph._element)
+	    	delete_mode = True
+    elif '</body' in paragraph.text:
+    	if '<body '+body_number in paragraph.text:
+    		keep_mode = False
+    		paragraph._element.getparent().remove(paragraph._element)
+    	else:
+	    	paragraph._element.getparent().remove(paragraph._element)
+	    	delete_mode = False
+    elif delete_mode:
+    		paragraph._element.getparent().remove(paragraph._element)
+    
+document.save('cover_letter.docx')
+
