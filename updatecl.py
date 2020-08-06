d4c73cb4541bd6d12797ae911ac4e6c0160a3d1a
Anthony Serrato <anthony17serrato@gmail.com>
2020-08-06 12:59:52 -0700

    script now displays available bodys for selection

--- updatecl.py
+++ updatecl.py
@@ -9,6 +9,11 @@ company_name =  str(raw_input("Enter the name of the company you are applying fo
 company_adress = str(raw_input("Enter the adress of the company you are applying for:\n"))
 position_name = str(raw_input("Enter the name of the position you are applying for:\n"))
 position_posting_location = str(raw_input("Where did you find the job posting?:\n"))
+print 'Available Bodies:\n'
+for paragraph in document.paragraphs:
+    if '<' in paragraph.text and '</' not in paragraph.text:
+        print paragraph.text+'\n'
+
 body_number = str(raw_input("Which preset cover letter body would you like to use?:(enter only the number ei. 1,2,3...)\n"))
 delete_mode = False
 keep_mode = False
